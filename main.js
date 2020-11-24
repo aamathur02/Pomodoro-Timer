c03bf26d5ca54ea1c179c47833a62f6af479ef95
Aayush Mathur <aamathur02@gmail.com>
2020-11-24 17:13:30 -0500

    added some more html, some js work remaining

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ const toggleClock = reset => {
 
 clockTimer = setInterval(() => {
     timeLeft--;
-    displaySessionTimeLeft();
+    stepDown();
 }, 1000);
 
 displaySessionTimeLeft = () => {
@@ -58,11 +58,45 @@ displaySessionTimeLeft = () => {
 }
 
 const stopClock = () => {
+    displaySessionLog(type);
     clearInterval(clockTimer);
     clockRunning = false;
     timeLeft = sessionTime;
-
+    timeSpentInCurrSession = 0;
     displaySessionTimeLeft();
 }
 
-let type = 'Work';
+let clockType = 'Work';
+
+const stepDown = () => {
+    if (timeLeft > 0) {
+        timeLeft--;
+        timeSpentInCurrSession++;
+    } else if (timeLeft === 0) {
+        timeSpentInCurrSession = 0;
+        if (clockType === 'Work') {
+            timeLeft = breakTime;
+            displaySessionLog('Work');
+            clockType = 'Break';
+        } else {
+            timeLeft = sessionTime;
+            clockType = 'Work';
+            displaySessionLog('Break')
+        }
+    }
+    displaySessionTimeLeft();
+}
+
+let timeSpentInCurrSession = 0;
+
+const displaySessionLog = type => {
+    const sessionsList = document.getElementById("sessions");
+    const newLi = document.createElement("li");
+    let sessionLabel = type;
+    let elapsedTime = parseInt(timeSpentInCurrSession / 60);
+    elapsedTime = elapsedTime > 0 ? elapsedTime : '< 1';
+
+    const text = document.createTextNode(`${sessionLabel} : ${elapsedTime} min`);
+    newLi.appendChild(text);
+    sessionsList.appendChild(newLi);
+}
